66d28acb190df4d98d20fb178ebd91e76ad8fb94
brad <brad@puppetlabs.vm>
2016-01-20 22:47:09 +0000

    move params into new class

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,51 +1,8 @@
 
 class nginx (
   $root = false
-) {
+) inherits nginx::params {
   # install nginx
-
-  case $::osfamily {
-    'redhat': {
-      $package_name = 'nginx'
-      $file_owner = 'root'
-      $file_group = 'root'
-      $document_root =  '/var/www'
-      $config_directory = '/etc/nginx'
-      $server_block_directory =  '/etc/nginx/conf.d'
-      $logs_directory = '/var/log/nginx'
-      $service_name = 'nginx'
-      $user_service_runs_as =  'nginx'
-    }
-    'debian': {
-      $package_name = 'nginx'
-      $file_owner = 'root'
-      $file_group = 'root'
-      $document_root =  '/var/www'
-      $config_directory = '/etc/nginx'
-      $server_block_directory =  '/etc/nginx/conf.d'
-      $logs_directory = '/var/log/nginx'
-      $service_name = 'nginx'
-      $user_service_runs_as =  'www-data'
-    }
-    'windows': {
-      $package_name = 'nginx-service'
-      $file_owner = 'Administrator'
-      $file_group = 'Administrators'
-      $document_root = 'C:/ProgramData/nginx/html'
-      $config_directory = 'C:/ProgramData/nginx/conf'
-      $server_block_directory = 'C:/ProgramData/nginx/conf.d'
-      $logs_directory = 'C:/ProgramData/nginx/logs'
-      $service_name = 'nginx'
-      $user_service_runs_as = 'nobody'
-    }
-    default: {
-      fail("${operatingsystem} not supported")
-    }
-  }
-
-  if $root {
-    $document_root = $root
-  }
 
   # set some defaults for the files
   File {
