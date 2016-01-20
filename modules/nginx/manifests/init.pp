3e436b4796dda929da0aa6c7b6aa6aeaa7622c14
brad <brad@puppetlabs.vm>
2016-01-20 01:01:32 +0000

    nginx: Lab 10.2

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -2,37 +2,92 @@
 class nginx {
   # install nginx
 
+  case $::osfamily {
+    'redhat': {
+      $package_name = 'nginx'
+      $file_owner = 'root'
+      $file_group = 'root'
+      $document_root =  '/var/www'
+      $config_directory = '/etc/nginx'
+      $server_block_directory =  '/etc/nginx/conf.d'
+      $logs_directory = '/var/log/nginx'
+      $service_name = 'nginx'
+      $user_service_runs_as =  'nginx'
+    }
+    'debian': {
+      $package_name = 'nginx'
+      $file_owner = 'root'
+      $file_group = 'root'
+      $document_root =  '/var/www'
+      $config_directory = '/etc/nginx'
+      $server_block_directory =  '/etc/nginx/conf.d'
+      $logs_directory = '/var/log/nginx'
+      $service_name = 'nginx'
+      $user_service_runs_as =  'www-data'
+    }
+    'windows': {
+      $package_name = 'nginx-service'
+      $file_owner = 'Administrator'
+      $file_group = 'Administrators'
+      $document_root = 'C:/ProgramData/nginx/html'
+      $config_directory = 'C:/ProgramData/nginx/conf'
+      $server_block_directory = 'C:/ProgramData/nginx/conf.d'
+      $logs_directory = 'C:/ProgramData/nginx/logs'
+      $service_name = 'nginx'
+      $user_service_runs_as = 'nobody'
+    }
+    default: {
+      fail("${operatingsystem} not supported")
+    }
+  }
+
   # set some defaults for the files
   File {
-    owner => 'root',
-    group => 'root',
+    owner => $file_owner,
+    group => $file_group,
     mode  => '0644',
   }
 
-  package { 'nginx':
+  package { $package_name:
     ensure => installed,
   }
 
-  file { '/var/www':
+  file { [
+    $document_root,
+    $config_directory,
+    $server_block_directory,
+    $logs_directory
+  ]:
     ensure => directory,
+    owner  => $file_owner,
+    group  => $file_group
   }
 
-  file { '/var/www/index.html':
+  file { "${document_root}/index.html":
     source => "puppet:///modules/nginx/index.html",
   }
 
-  file { '/etc/nginx/nginx.conf':
-    source => "puppet:///modules/nginx/nginx.conf",
+  file { "${config_directory}/nginx.conf":
+    source => [
+      "puppet:///modules/nginx/${::osfamily}.conf",
+      "puppet:///modules/nginx/nginx.conf",
+    ],
     notify => Service['nginx'],
   }
 
-  file { '/etc/nginx/conf.d/default.conf':
-    source => "puppet:///modules/nginx/default.conf",
+  # try $::kernel first, filename is ...-Linux.
+  file { "${config_directory}/conf.d/default.conf":
+    source => [
+      "puppet:///modules/nginx/default-${::kernel}.conf",
+      "puppet:///modules/nginx/default-${::osfamily}.conf",
+      "puppet:///modules/nginx/default.conf",
+    ],
     notify => Service['nginx'],
   }
 
-  service { 'nginx':
+  service { $service_name:
     ensure => running,
     enable => true,
+
   }
 }
